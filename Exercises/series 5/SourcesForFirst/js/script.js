3a4a818a6cba4788d15a9f3d0e4fdc3e88034ace
Aria <151901055+Ariashakoo@users.noreply.github.com>
2025-01-04 21:37:37 +0330

    Last series

---
+++ Exercises/series 5/SourcesForFirst/js/script.js
@@ -0,0 +1,21 @@
+﻿var btn = document.getElementById('btn');
+var bub = document.getElementById('bulb');
+
+var flag = false;
+
+btn.addEventListener('click' , turnOffOrOn);
+
+function turnOffOrOn() {
+    
+
+    if (flag) {
+        flag = false;
+        btn.innerHTML = 'turn on';
+        bub.setAttribute('src','pics/bulboff.gif');
+    }
+    else {
+        flag = true;
+        btn.innerHTML = 'turn off';
+        bub.setAttribute('src','pics/bulbon.gif');
+    }
+}
